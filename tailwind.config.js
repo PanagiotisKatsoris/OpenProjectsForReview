db1a9b872759f3787a23635aaf6defa85267468d
PanagiotisKatsoris <katsoris21@gmail.com>
2025-01-14 23:02:23 +0200

    minor changes to start styling

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  corePlugins: {
+    preflight: false,
+  },
+  prefix: 'tw-',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
